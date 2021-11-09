d97824a2211cf9a03b45d55c9f445fbf95ca9c7a
Piotr <piotr.gawronski@bitpanda.com>
2021-11-09 17:04:53 +0100

    go semaphore implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/spayder/semaphore
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"sync"
+)
+
+type Task struct {
+	ID        int    `json:"id"`
+	UserID    int    `json:"user_id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+	var t Task
+	wg := &sync.WaitGroup{}
+	wg.Add(100)
+	sem := make(chan bool, 10)
+	for i := 0; i < 100; i++ {
+		fmt.Println(runtime.NumGoroutine())
+		sem <- true
+		go func(i int) {
+			defer wg.Done()
+			defer func() { <-sem }()
+			res, err := http.Get(fmt.Sprintf("https://jsonplaceholder.typicode.com/todos/%d", i))
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(t.Title)
+		}(i)
+	}
+	wg.Wait()
+}
